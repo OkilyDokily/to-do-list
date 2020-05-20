e83d107f1258f18baf6abda9eb17c97d7d20c928
me <me@me.me.com>
2020-05-20 14:19:04 -0700

    slightly update the spec

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function todoList(){
+  var counter = 0;
+  this.todos = [];
+}
+
+todoList.prototype.addTodo = function(todo){
+  todo.id = ++counter;
+  this.todos.push(todo);
+}
+
+function todo(string){
+  this.string = string;
+}
+
+convertStringToTodoObject(string){
+  return new todo(string); 
+}
+
+
+$(document).ready(function{}){
+
+}
